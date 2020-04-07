2cef1e621830dd2bd563d9bc43a351a00242f46d
JorgeQZ <jorge_qzg@hotmail.com>
2020-04-07 11:56:05 -0500

    Filtros de busqueda html-css

--- page-home.php
+++ page-home.php
@@ -7,7 +7,90 @@
 ?>
 
 <?php get_header();?>
+<!-- Banner -->
 <div class="hero-banner">
     HERO BANNER
-</div>
+</div><!-- Banner -->
+
+<!-- Filtros -->
+<div class="filter-container">
+    <div class="filter-title">
+        Encuentra el departamento
+        <div class="focus">
+            que estabas buscando.
+        </div>
+    </div>
+
+    <!-- Filters -->
+    <div class="filters">
+        <!-- col -->
+        <div class="col">
+            <div class="filter-cont">
+                <div class="value">Ubicación </div>
+                <ul>
+                    <li>Test 1</li>
+                    <li>Test 2</li>
+                    <li>Test 3</li>
+                </ul>
+            </div>
+        </div> <!-- col -->
+
+        <!-- col -->
+        <div class="col">
+            <div class="filter-cont">
+                <div class="value">Nº de recamaras </div>
+                <ul>
+                    <li>Test 1</li>
+                    <li>Test 2</li>
+                    <li>Test 3</li>
+                </ul>
+            </div>
+        </div> <!-- col -->
+    </div><!-- Filters -->
+    <!-- Filters -->
+    <div class="filters">
+        <!-- col -->
+
+        <div class="col">
+            <div class="filter-cont">
+                <div class="value">Baños </div>
+                <ul>
+                    <li>Test 1</li>
+                    <li>Test 2</li>
+                    <li>Test 3</li>
+                </ul>
+            </div>
+        </div> <!-- col -->
+
+        <!-- col -->
+        <div class="col">
+            <div class="filter-cont">
+                <div class="value">rango de precios </div>
+
+                <ul>
+                    <li>Test 1</li>
+                    <li>Test 2</li>
+                    <li>Test 3</li>
+                </ul>
+            </div>
+        </div> <!-- col -->
+
+    </div><!-- Filters -->
+
+    <!-- Busqueda avanzada -->
+    <div class="advanced-search">
+        <div class="title">
+            Busqueda avanzada
+            <div class="little-arrow"></div>
+        </div>
+    </div> <!-- Busqueda avanzada -->
+
+    <!-- Boton buscar -->
+    <div class="filter-submit">
+        <div class="submit">Buscar</div>
+    </div><!-- Boton buscar -->
+
+</div><!-- Filtros -->
+
+
 <?php get_footer();?>
