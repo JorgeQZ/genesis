350f598ebce20f5bae5c43bb87a7aad26811072d
Byron-Guevara <byronguevara@akevia.mx>
2020-06-10 12:30:29 -0500

    Desarrollo Ciudad

--- page-desarrollo-ciudad.php
+++ page-desarrollo-ciudad.php
@@ -7,8 +7,9 @@ get_header(); ?>
 
 <div class="contenedor-general-desarrollos">
 
-    <div class="contenedor-general-desarrollo" style="background-image: url(<?php echo get_template_directory_uri().'/img/fondo-ancora.jpg'?>;">
+    <div class="contenedor-general-desarrollo">
         <div class="contenedor-desarrollo">
+            <img class="logo-esq" src="<?php echo get_template_directory_uri().'/img/ancora.png'?>" alt="">
             <div class="cont-info">
             <!--    <img src="<?php echo get_template_directory_uri().'/img/ancora.png'?>" alt=""> -->
                 <div class="info">
@@ -33,7 +34,7 @@ get_header(); ?>
                             <p>2 Est.</p>
                         </div>
                     </div>
-                    <a href="#">ver desarrollo</a>
+                    <a class="link-desarrollo" href="#">ver desarrollo</a>
                 </div>
 
             </div>
@@ -47,6 +48,144 @@ get_header(); ?>
                     </div>
                     <div class="item">
                         <img src="<?php echo get_template_directory_uri().'/img/ancora-slide.jpg'?>" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="contenedor-general-desarrollo">
+        <div class="contenedor-desarrollo">
+            <img class="logo-esq" src="<?php echo get_template_directory_uri().'/img/montalva.png'?>" alt="">
+            <div class="cont-info">
+                <div class="info">
+                    <h1>Montalva Residencial</h1>
+                    <p>Precios Desde: <span>$3.000.000</span></p>
+                    <p class="ubicacion">  Av. Sor Juana Inés de la Cruz 312, <br> Tlalnepantla Estado de México.</p>
+                    <div class="detalles">
+                        <div>
+                            <img src="<?php echo get_template_directory_uri().'/img/icon-size.png'?>" alt="">
+                            <p>5201m</p> 
+                        </div>
+                        <div>
+                            <img src="<?php echo get_template_directory_uri().'/img/icon-bath.png'?>" alt="">
+                            <p>Desde <span>1</span> a <span>3</span> Baños</p> 
+                        </div>
+                        <div>
+                            <img src="<?php echo get_template_directory_uri().'/img/icon-bed.png'?>" alt="">
+                            <p>Desde <span>2</span> a <span>4</span> Recámaras</p> 
+                        </div>
+                        <div>
+                            <img src="<?php echo get_template_directory_uri().'/img/icon-car.png'?>" alt=""> 
+                            <p>2 Est.</p>
+                        </div>
+                    </div>
+                    <a class="link-desarrollo" href="#">ver desarrollo</a>
+                </div>
+
+            </div>
+            <div class="cont-media">
+                <div class="desarrollo-slider owl-carousel owl-theme">
+                    <div class="item">
+                        <img src="<?php echo get_template_directory_uri().'/img/montalva-slide.jpg'?>" alt="">
+                    </div>
+                    <div class="item">
+                        <img src="<?php echo get_template_directory_uri().'/img/montalva-slide.jpg'?>" alt="">
+                    </div>
+                    <div class="item">
+                        <img src="<?php echo get_template_directory_uri().'/img/montalva-slide.jpg'?>" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="contenedor-general-desarrollo">
+        <div class="contenedor-desarrollo">
+            <img class="logo-esq" src="<?php echo get_template_directory_uri().'/img/tlanepantla.png'?>" alt="">
+            <div class="cont-info">
+                <div class="info">
+                    <h1>Puerta Tlanepantla</h1>
+                    <p>Precios Desde: <span>$3.000.000</span></p>
+                    <p class="ubicacion">   Av. Sor Juana Inés de la Cruz 312, <br> Tlalnepantla Estado de México.</p>
+                    <div class="detalles">
+                        <div>
+                            <img src="<?php echo get_template_directory_uri().'/img/icon-size.png'?>" alt="">
+                            <p>5201m</p> 
+                        </div>
+                        <div>
+                            <img src="<?php echo get_template_directory_uri().'/img/icon-bath.png'?>" alt="">
+                            <p>Desde <span>1</span> a <span>3</span> Baños</p> 
+                        </div>
+                        <div>
+                            <img src="<?php echo get_template_directory_uri().'/img/icon-bed.png'?>" alt="">
+                            <p>Desde <span>2</span> a <span>4</span> Recámaras</p> 
+                        </div>
+                        <div>
+                            <img src="<?php echo get_template_directory_uri().'/img/icon-car.png'?>" alt=""> 
+                            <p>2 Est.</p>
+                        </div>
+                    </div>
+                    <a class="link-desarrollo" href="#">ver desarrollo</a>
+                </div>
+
+            </div>
+            <div class="cont-media">
+                <div class="desarrollo-slider owl-carousel owl-theme">
+                    <div class="item">
+                        <img src="<?php echo get_template_directory_uri().'/img/tlanepantla-slide.jpg'?>" alt="">
+                    </div>
+                    <div class="item">
+                        <img src="<?php echo get_template_directory_uri().'/img/tlanepantla-slide.jpg'?>" alt="">
+                    </div>
+                    <div class="item">
+                        <img src="<?php echo get_template_directory_uri().'/img/tlanepantla-slide.jpg'?>" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="contenedor-general-desarrollo">
+        <div class="contenedor-desarrollo">
+            <img class="logo-esq" src="<?php echo get_template_directory_uri().'/img/bolognia.png'?>" alt="">
+            <div class="cont-info">
+                <div class="info">
+                    <h1>Bolognia Residencial</h1>
+                    <p>Precios Desde: <span>$3.000.000</span></p>
+                    <p class="ubicacion">  Av. Sor Juana Inés de la Cruz 312, <br> Tlalnepantla Estado de México.</p>
+                    <div class="detalles">
+                        <div>
+                            <img src="<?php echo get_template_directory_uri().'/img/icon-size.png'?>" alt="">
+                            <p>5201m</p> 
+                        </div>
+                        <div>
+                            <img src="<?php echo get_template_directory_uri().'/img/icon-bath.png'?>" alt="">
+                            <p>Desde <span>1</span> a <span>3</span> Baños</p> 
+                        </div>
+                        <div>
+                            <img src="<?php echo get_template_directory_uri().'/img/icon-bed.png'?>" alt="">
+                            <p>Desde <span>2</span> a <span>4</span> Recámaras</p> 
+                        </div>
+                        <div>
+                            <img src="<?php echo get_template_directory_uri().'/img/icon-car.png'?>" alt=""> 
+                            <p>2 Est.</p>
+                        </div>
+                    </div>
+                    <a class="link-desarrollo" href="#">ver desarrollo</a>
+                </div>
+
+            </div>
+            <div class="cont-media">
+                <div class="desarrollo-slider owl-carousel owl-theme">
+                    <div class="item">
+                        <img src="<?php echo get_template_directory_uri().'/img/bolognia-slide.jpg'?>" alt="">
+                    </div>
+                    <div class="item">
+                        <img src="<?php echo get_template_directory_uri().'/img/bolognia-slide.jpg'?>" alt="">
+                    </div>
+                    <div class="item">
+                        <img src="<?php echo get_template_directory_uri().'/img/bolognia-slide.jpg'?>" alt="">
                     </div>
                 </div>
             </div>
